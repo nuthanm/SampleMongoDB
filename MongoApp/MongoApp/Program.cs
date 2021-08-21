2475dbd5ad96637435d2cd6bb2b3df45f516db9c
nuthan <inbox.nuthan@gmail.com>
2021-08-21 16:35:01 +0530

    Initial MongoDB Project commit

---
+++ MongoApp/MongoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
