0aa475d0b7c0c2b9a867a8fccec967bdc0e2e659
nuthan <inbox.nuthan@gmail.com>
2021-09-01 01:00:57 +0530

    Generic Method: Merge (Insert or Update). If id matches with document Id from collection then update or else insert.

--- MongoApp/MongoApp/MongoCRUD.cs
+++ MongoApp/MongoApp/MongoCRUD.cs
@@ -55,5 +55,24 @@ namespace MongoApp
             var filter = Builders<T>.Filter.Eq("Id", id);
             return collection.Find(filter).FirstOrDefault();
         }
+
+        /// <summary>
+        /// Either update or insert the record into the collection.
+        /// If id is not match and based on IsUpsert = true then it inserts
+        /// If id is match then record is going to update.
+        /// </summary>
+        /// <typeparam name="T">T</typeparam>
+        /// <param name="table">table</param>
+        /// <param name="id">id</param>
+        /// <param name="record">record</param>
+        public void UpdateRecord<T>(string table, Guid id, T record)
+        {
+            var collection = _db.GetCollection<T>(table);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", id),
+                record,
+                new ReplaceOptions { IsUpsert = true });
+        }
     }
 }
