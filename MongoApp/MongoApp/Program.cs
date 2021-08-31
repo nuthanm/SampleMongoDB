00f941dec4d61df59e7d05cf9f7d69689b67b51b
nuthan <inbox.nuthan@gmail.com>
2021-09-01 01:17:35 +0530

    Write a delete call after updated a record with new values.

--- MongoApp/MongoApp/Program.cs
+++ MongoApp/MongoApp/Program.cs
@@ -13,25 +13,25 @@ namespace MongoApp
             // db.InsertRecord<UserDetails>("horoscope", new UserDetails { GroomName = "Nani", BrideName = "Potti" });
 
             // Option 2: Insert full record
-            //var userWithHoroscope = new UserDetails
-            //{
-            //    GroomName = "Nani",
-            //    BrideName = "Potti",
-            //    BrideDOB = "1993-12-20",
-            //    GroomDOB = "1987-10-29",
-            //    BridePlaceOfBirth = "Kakinada",
-            //    GroomPlaceOfBirth = "Nellore",
-            //    BrideTime = "2:00 PM",
-            //    GroomTime = "5:00 PM",
-            //    horoscope = new HoroscopeDetails()
-            //    {
-            //        MatchCount = 29,
-            //        MatchStatus = "Perfect",
-            //        Reason = "Sooper ga chesukovachu",
-            //    }
-            //};
+            var userWithHoroscope = new UserDetails
+            {
+                GroomName = "Nani",
+                BrideName = "Potti",
+                BrideDOB = "1993-12-20",
+                GroomDOB = "1987-10-29",
+                BridePlaceOfBirth = "Kakinada",
+                GroomPlaceOfBirth = "Nellore",
+                BrideTime = "2:00 PM",
+                GroomTime = "5:00 PM",
+                horoscope = new HoroscopeDetails()
+                {
+                    MatchCount = 29,
+                    MatchStatus = "Perfect",
+                    Reason = "Sooper ga chesukovachu",
+                }
+            };
 
-            //db.InsertRecord<UserDetails>("horoscope", userWithHoroscope);
+            db.InsertRecord<UserDetails>("horoscope", userWithHoroscope);
             //// var newUserId = Guid.NewGuid();
             // var updateUser = new UserDetails
             // {
@@ -64,13 +64,16 @@ namespace MongoApp
             }
 
             // var userRec = db.LoadRecordById<UserDetails>("horoscope", new Guid("1cf60e95-20aa-40cf-a0b2-0722cd42884b"));
-            var userRec = db.LoadRecordById<UserDetails>("horoscope", new Guid("317565d8-a055-4348-a05c-00e5836b8b9b"));
+            var userRec = db.LoadRecordById<UserDetails>("horoscope", new Guid("d7cdef35-ef7a-46d4-bf24-0abb7d02f1cd"));
             userRec.BrideName = "Sobha";
             userRec.GroomName = "Nuthan";
-            db.UpdateRecord<UserDetails>("horoscope", new Guid("317565d8-a055-4348-a05c-00e5836b8b9b"), userRec);
+            db.UpdateRecord<UserDetails>("horoscope", userRec.Id, userRec);
 
             Console.WriteLine($"Horoscope status: {userRec?.horoscope?.MatchStatus} and reason: {userRec?.horoscope?.Reason}");
-            
+
+            // db.DeleteRecord<UserDetails>("horoscope", userRec.Id);
+            // Console.WriteLine("Record deleted successfully.");
+                    
             Console.ReadKey();
         }
     }
